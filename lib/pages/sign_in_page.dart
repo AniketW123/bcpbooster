cbcf95d64c5511f93a5c24ec16213d86e94a9dd4
Shaunak <shaunak.warty@gmail.com>
2020-05-25 22:55:42 -0700

    Add link to privacy policy

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 import 'page.dart';
 import 'start_page.dart';
 import '../constants.dart';
@@ -88,33 +89,54 @@ class _SignInPageState extends PageState<SignInPage> {
 
   @override
   Widget buildBody(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 50.0),
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            TitleText('BCP Booster Club'),
-            _bodyText([
-              'A center for managing members for Bellarmine College Prep\'s Booster Club Program.',
-              'The Booster Club is a great way to get free tickets to sporting events and BCP merchandise.',
-              'Booster Club volunteers can use this website to add new members and search for/update the status of existing members'
-            ]),
-            Padding(
-              padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                TitleText('BCP Booster Club'),
+                _bodyText([
+                  'A center for managing members for Bellarmine College Prep\'s Booster Club Program.',
+                  'The Booster Club is a great way to get free tickets to sporting events and BCP merchandise.',
+                  'Booster Club volunteers can use this website to add new members and search for/update the status of existing members'
+                ]),
+                Padding(
+                  padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
+                  child: Text(
+                    'Volunteer Login',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                ),
+                GoogleSignInButton(
+                  onPressed: googleSignIn.signIn,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0.0,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+            child: GestureDetector(
               child: Text(
-                'Volunteer Login',
+                'Privacy Policy',
                 style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w600
+                  decoration: TextDecoration.underline
                 ),
               ),
+              onTap: () {
+                launch('https://booster.belldcb.com/privacy');
+              },
             ),
-            GoogleSignInButton(
-              onPressed: googleSignIn.signIn,
-            ),
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
